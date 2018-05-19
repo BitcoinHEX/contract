bc5afadd3525d670deef3dc78569c4d6983a6015
Kieran Mesquita <mesquitakieran@gmail.com>
2018-05-19 19:26:29 +0800

    Zerod Inital Supply

--- src/BitcoinHex.sol
+++ src/BitcoinHex.sol
@@ -1,15 +1,12 @@
 pragma solidity ^0.4.23;
-import "./token/UTXORedeemableToken.sol";
+import "./token/StakeableToken.sol";
 
-contract BitcoinHex is UTXORedeemableToken {
+contract BitcoinHex is StakeableToken {
     string public name = "BitcoinHex"; 
     string public symbol = "BHX";
     uint public decimals = 18;
-    uint public INITIAL_SUPPLY = 10000 * (10 ** decimals);
-    uint256 public totalSupply;
 
     constructor () public {
-        totalSupply = INITIAL_SUPPLY;
-        balances[msg.sender] = INITIAL_SUPPLY;
+        totalSupply_ = 0;
     }
 }
