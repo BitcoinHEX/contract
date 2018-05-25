6d595370b9f3765a7a5b39d2329b5c5ce6a4a0a6
Kieran Mesquita <mesquitakieran@gmail.com>
2018-05-25 11:34:53 +0800

    Linting and commit build

--- contracts/BitcoinHex.sol
+++ contracts/BitcoinHex.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.23;
 import "./StakeableToken.sol";
-import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract BitcoinHex is StakeableToken, Ownable {
     string public name = "BitcoinHex"; 
--- contracts/UTXORedeemableToken.sol
+++ contracts/UTXORedeemableToken.sol
@@ -18,7 +18,7 @@
 
 pragma solidity ^0.4.23;
 
-import 'zeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
+import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 import "./MerkleProof.sol";
 
 /**
