eb7fbf7456d63a76693575a4020aa1c397f43fe2
Kieran Mesquita <mesquitakieran@gmail.com>
2018-05-31 03:13:47 +0800

    Initialising Variables

--- contracts/BitcoinHex.sol
+++ contracts/BitcoinHex.sol
@@ -5,11 +5,14 @@ import "zeppelin-solidity/contracts/ownership/Ownable.sol";
 contract BitcoinHex is StakeableToken, Ownable {
     string public name = "BitcoinHex"; 
     string public symbol = "BHX";
-    uint public decimals = 18;
+    uint public decimals = 8;
 
     constructor () public {
         totalSupply_ = 0;
         launchTime = block.timestamp;
         owner = msg.sender;
+        rootUTXOMerkleTreeHash = 0x0; // Change before launch
+        maximumRedeemable = 0; // Change before launch
+        
     }
 }
