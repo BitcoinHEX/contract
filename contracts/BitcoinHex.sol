fb7d6925fe27a500d3b780f6378d701acafbe0f3
Kieran Mesquita <mesquitakieran@gmail.com>
2018-10-24 12:08:23 +0400

    Cleaning House

--- contracts/BitcoinHex.sol
+++ contracts/BitcoinHex.sol
@@ -1,33 +1,8 @@
 pragma solidity ^0.4.23;
 import "./StakeableToken.sol";
 
-
-/**
-  @title BitcoinHex token contract
-  @notice inherits from StakeableToken which handles staking rewards, 
-  UTXORedeemableToken which handles redeeming tokens through OpenZeppelin
-  MerkleProof, and StandardToken (also from OpenZeppelin)
-
-  @dev _rootUtxoMerkleTreeHash is derived from the Bitcoin blockchain
-  but is not an exact copy.
-*/
 contract BitcoinHex is StakeableToken {
-  string public name = "BitcoinHex"; 
-  string public symbol = "BHX";
-  uint public decimals = 18;
+  constructor () public {
 
-  constructor (
-    address _originAddress,
-    bytes32 _rootUtxoMerkleTreeHash,
-    uint256 _maximumRedeemable,
-    uint256 _UTXOCountAtFork
-  ) 
-    public
-  {
-    launchTime = block.timestamp;
-    origin = _originAddress;
-    rootUtxoMerkleTreeHash = _rootUtxoMerkleTreeHash;
-    maximumRedeemable = _maximumRedeemable;
-    UTXOCountAtFork = _UTXOCountAtFork;
   }
 }
---
+++ contracts/GlobalsAndUtility.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.4.23;
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+
+contract GlobalsAndUtility is StandardToken {
+
+}
